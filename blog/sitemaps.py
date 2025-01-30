27c8b8229565afa18a25f5e736a5d9a9e6e3f085
abeni-al7 <abenezeralebachew3@gmail.com>
2025-01-30 15:33:42 +0300

    Add tags to the sitemap

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,4 +1,6 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from taggit.models import Tag
 from .models import Post
 
 
@@ -11,3 +13,22 @@ class PostSitemap(Sitemap):
 
     def lastmod(self, obj):
         return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Get tags used in published posts
+        return Tag.objects.filter(
+            pk__in=Post.published.values_list("tags", flat=True)
+        ).distinct()
+
+    def location(self, tag):
+        return reverse("blog:post_list_by_tag", kwargs={"tag_slug": tag.slug})
+
+    # Optional: Add last modification date (using latest post's update time)
+    def lastmod(self, tag):
+        latest_post = Post.published.filter(tags=tag).order_by("-updated").first()
+        return latest_post.updated if latest_post else None
