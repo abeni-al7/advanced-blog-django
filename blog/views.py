db1a46520a1b356350ec04b5bdbf467102272eb3
abeni-al7 <abenezeralebachew3@gmail.com>
2025-01-30 15:12:03 +0300

    Use postgres Trigram extention to support a more robust search functionality based on similarity of 3 consecutive words allowing typos to be supported

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -136,16 +136,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", weight="A") + SearchVector(
-                "body", weight="B"
-            )
-            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity("title", query),
                 )
-                .filter(rank__gte=0.3)
-                .order_by("-rank")
+                .filter(similarity__gte=0.1)
+                .order_by("-similarity")
             )
 
     return render(
